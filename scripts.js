30df281f03c1199ebf9b77e552573a5889ed4c5a
Fr4nnc0x <fagl.work@gmail.com>
2024-02-18 16:47:28 -0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,19 @@
+//alert("Hola este es mi Javascript");
+
+let nombre = "Franco";
+
+var nombre1 = "Franco";
+
+const nombre2 = "Franco";
+
+console.log(nombre);
+console.log(nombre1);
+console.log(nombre2);
+
+
+let textoTitulo = titulo.innerText;
+console.log(textoTitulo);
+
+if(textoTitulo == "rutrum"){
+    titulo.innerHTML = "Otro"
+}
